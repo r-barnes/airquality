7a79e9103378d49e1cbf4598cd91b8b89c9194ea
Richard Barnes <rbarnes@umn.edu>
2014-06-21 14:03:30 -0500

    Generalized the code to work with multiple systems

--- db_loaders/airnow.py
+++ db_loaders/airnow.py
@@ -6,75 +6,115 @@ import psycopg2
 import sys
 import io
 
-#encoding = locale.getdefaultlocale()[1]
-
-db    = psycopg2.connect(database='airquality', user='airq', password='nach0s', host='localhost')
-db.set_isolation_level(0) #Set conneciton to autocommit
-dbcur = db.cursor()
-
-dbcur.execute("PREPARE measurement_insert AS INSERT INTO measurements (stationid,	system,	datetime,	param, value) VALUES ($1, $2, $3, $4, $5)")
-
-print("Connecting via FTP...")
-ftp   = ftplib.FTP(host='ftp.airnowapi.org', user='leapingleopard', passwd='xi7MxOiwrRR_vKeT')
-
-ftp.cwd('HourlyData')
-
-print("Retrieving file list...")
-files=list(ftp.mlsd('*dat'))
-#files=[('bob')]
-files.sort(key=lambda x: x[0], reverse=True)
-
 paramkeys = {'OZONE': 1, 'PM10': 2, 'PM2.5': 3, 'TEMP':4, 'BARPR': 5, 'SO2': 6, 'RHUM': 7, 'WS': 8, 'WD': 9, 'CO': 10, 'NOY': 11, 'NO2Y': 12, 'NO': 13, 'NOX': 14, 'NO2': 15, 'PRECIP': 16, 'SRAD': 17, 'BC': 18, 'EC': 19, 'OC': 20}
 
-for f in files[0:]:
-	filename = f[0]
-	data     = []
-	print("Retrieving file " + filename)
-	ftp.retrlines('RETR '+filename, data.append)
+class MeasureMonkey:
+	def __init__(self, system):
+		self.db    = psycopg2.connect(database='airquality', user='airq', password='nach0s', host='localhost')
+		self.db.set_isolation_level(0) #Set conneciton to autocommit
 
-	#with open('../data/2014052706.dat', 'rb') as fin:
-#		data=fin.readlines()
-#	data = map(lambda x: x.strip(), data)
+		self.meascursor = self.db.cursor()
+		self.meascursor.execute("PREPARE measurement_insert AS INSERT INTO measurements (stationid,	system,	datetime,	param, value) VALUES ($1, $2, $3, $4, $5)")
 
-	#"valid_date", "valid_time", "aqsid", "sitename", "GMT_offset", "parameter_name", "reporting_units", "value", "data_source"
-	print('Parsing data...')
-	data_to_insert = set()
-	for d in data:
-		#try:
-	#		d         = d.decode(encoding)
-#		except:
-	#		continue
-		d=d.strip()
-		if len(d)==0:
-			continue
-		d         = d.split('|')
-		stationid = d[2]
-		system    = 1
-		thedate   = d[0] +' '+ d[1] #TODO: What format is this?
+		self.data_to_insert = set()
+		self.system = system
 
-		param     = d[5]
-		if param in paramkeys:
-			param=paramkeys[param]
+	def insert(self, stationid, thedate, param, value):
+		datum = (stationid, self.system, thedate, param, value)
+		self.data_to_insert.add(datum)
+
+	def commit(self):
+		prepped_data = ["\t".join([str(y) for y in x]) for x in self.data_to_insert]
+		prepped_data = "\n".join(prepped_data)
+		prepped_data = io.StringIO(prepped_data)
+		prepped_data.seek(0)
+		self.meascursor.copy_from(prepped_data, 'measurements')
+		self.data_to_insert = set()
+
+
+class AirNow:
+	def __init__(self):
+		pass
+
+	def _connectFTP(self):
+		print("Connecting via FTP...")
+		self.ftp = ftplib.FTP(host='ftp.airnowapi.org', user='leapingleopard', passwd='xi7MxOiwrRR_vKeT')
+
+	def loadStations(self):
+		pass
+
+	def loadData(self, command):
+		db = MeasureMonkey(1)
+
+		self._connectFTP()
+		self.ftp.cwd('HourlyData')
+
+		print("Retrieving file list...")
+		files=list(self.ftp.mlsd('*dat'))
+		files.sort(key=lambda x: x[0], reverse=True)
+
+		if   command=='latestonline':
+			files = files[0:1]
+		elif command=='bulkonline':
+			pass
 		else:
-			sys.stderr.write("Unknown param key: %s\n" % (param))
-			continue
+			raise Exception("Onrecognised command")
 
-		value     = d[7]
+		for f in files:
+			filename = f[0]
+			data     = []
+			print("Retrieving file " + filename)
+			self.ftp.retrlines('RETR '+filename, data.append)
 
-		datum = (stationid, system, thedate, param, value)
-		data_to_insert.add(datum)
+			#These are the CSV file headers
+			#"valid_date", "valid_time", "aqsid", "sitename", "GMT_offset", "parameter_name", "reporting_units", "value", "data_source"
+			print('Parsing data...')
+			for d in data:
+				d=d.strip()
+				if len(d)==0:
+					continue
+				d         = d.split('|')
+				stationid = d[2]
+				thedate   = d[0] +' '+ d[1] #TODO: What format is this?
 
-	data_to_insert=["\t".join([str(y) for y in x]) for x in data_to_insert]
-	data_to_insert="\n".join(data_to_insert)
+				param     = d[5]
+				if param in paramkeys:
+					param = paramkeys[param]
+				else:
+					sys.stderr.write("Unknown param key: %s\n" % (param))
+					continue
 
-	data_to_insert=io.StringIO(data_to_insert)
-	data_to_insert.seek(0)
-	dbcur.copy_from(data_to_insert, 'measurements')
+				value     = d[7]
 
-	#try:
-		#dbcur.execute("INSERT INTO measurements (stationid,	system,	datetime,	param, value) VALUES (%s, %s, %s, %s, %s)", data_to_insert)
-	#	dbcur.execute("EXECUTE measurement_insert (%s, %s, %s, %s, %s)", data_to_insert)
-	#except psycopg2.IntegrityError as excp:
-#		print(excp.pgerror)
+				db.insert(stationid, thedate, param, value)
 
-	db.commit()
+			db.commit()
+
+
+
+
+if len(sys.argv)==1:
+	print("Syntax: %s <API NAME> <COMMAND>" % (sys.argv[0]))
+	print("\nAPIS:")
+	print("\tairnow dataload <FILE>")
+	print("\tairnow dataload latestonline")
+	print("\tairnow dataload bulkonline")
+	print("\tairnow stationload <FILE>")
+	print("\tairnow stationload online")
+	sys.exit(-1)
+
+apis = {'airnow': AirNow}
+
+if not sys.argv[1] in apis:
+	print("API not found")
+	sys.exit(-1)
+else:
+	api = apis[sys.argv[1]]()
+	if len(sys.argv)!=4:
+		raise Exception('Need a command and argument')
+	elif sys.argv[2]=='dataload':
+		api.loadData(sys.argv[3])
+	elif sys.argv[2]=='stationload':
+		api.loadStations(sys.argv[3])
+	else:
+		raise Exception('Unrecognised command')
