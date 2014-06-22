8448e007d9065b74f5a8faa00c8b4eb490c36c01
Richard Barnes <rbarnes@umn.edu>
2014-06-22 12:41:05 -0500

    Use POSTGRES for zips

--- zipcode_format.py
+++ zipcode_format.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import csv
-import redis
+import psycopg2
 import json
+import io
 
-r_server = redis.Redis("localhost")
+db       = psycopg2.connect(database='airquality', user='airq', password='nach0s', host='localhost')
+db.set_isolation_level(0) #Set conneciton to autocommit
+dbcursor = db.cursor()
 
 input_file = csv.DictReader(open("zipcode.csv"))
 
+prepped_data = ""
 for row in input_file:
   row['latitude']  = float(row['latitude'])
   row['longitude'] = float(row['longitude'])
   row['timezone']  = int(row['timezone'])
 
-  jdump = json.dumps(row, ensure_ascii=False)
-  r_server.set("zip" + row['zip'], jdump)
+  jdump         = json.dumps(row, ensure_ascii=False)
+  prepped_data += row['zip']+"\t"+jdump+"\n"
+
+prepped_data = io.StringIO(prepped_data)
+prepped_data.seek(0)
+dbcursor.copy_from(prepped_data, 'zips')
